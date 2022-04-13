9591d851bd45ce8e979881a173f1b13fa9d281ee
leonardo-terra <leo.terra.melo@gmail.com>
2022-04-12 22:27:30 -0300

    dev: render Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import "./index.css";
+import Header from "./components/Header";
 
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className=''>
+      <Header />
+      <span>hello world!</span>
+    </div>
+  );
 }
