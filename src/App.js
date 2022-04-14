9c004155cccc0ee9a83aef896e05d408cc1cbe3e
leonardo-terra <leo.terra.melo@gmail.com>
2022-04-14 19:39:17 -0300

    dev: create about me page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./index.css";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 export default function App() {
   return (
     <div className=''>
       <Header />
       <div className="min-h-screen bg-fixed bg-cover bg-[url('./images/background-placeholder.webp')]" />
+      <AboutMe />
       <span>hello world!</span>
     </div>
   );
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,10 @@
+export default function AboutMe() {
+  return (
+    <>
+      <h1 className='p-10 tracking-wider text-center text-sky-400 '>
+        About me
+      </h1>
+      <span> Vou contar um pouco mais sobre meu trabalho...</span>
+    </>
+  );
+}
